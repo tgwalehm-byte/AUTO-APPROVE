58ff51753518727197a4dbbea33f9c7845705cee
tgwalehm-byte <tgwalehm@gmail.com>
2026-09-08 09:13:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -16,6 +16,8 @@ db = mongo["join_request_bot"]
 settings_collection = db["settings"]
 ads_collection = db["ads"]
 requests_collection = db["requests"]
+users_collection = db["users"]
+groups_collection = db["groups"]
 
 
 # =========================================================
@@ -31,7 +33,9 @@ async def get_auto_approve(chat_id: int) -> bool:
     if not data:
         return False
 
-    return bool(data.get("auto_approve", False))
+    return bool(
+        data.get("auto_approve", False)
+    )
 
 
 async def set_auto_approve(
@@ -85,6 +89,113 @@ async def delete_ad():
     await ads_collection.delete_one(
         {"_id": "global_ad"}
     )
+
+
+# =========================================================
+# USERS
+# =========================================================
+
+async def save_user(
+    user_id: int
+):
+
+    await users_collection.update_one(
+        {
+            "user_id": user_id
+        },
+        {
+            "$set": {
+                "user_id": user_id
+            }
+        },
+        upsert=True
+    )
+
+
+async def get_all_users():
+
+    cursor = users_collection.find(
+        {},
+        {
+            "_id": 0,
+            "user_id": 1
+        }
+    )
+
+    return await cursor.to_list(
+        length=None
+    )
+
+
+async def delete_user(
+    user_id: int
+):
+
+    await users_collection.delete_one(
+        {
+            "user_id": user_id
+        }
+    )
+
+
+async def get_total_users():
+
+    return await users_collection.count_documents({})
+
+
+# =========================================================
+# GROUPS
+# =========================================================
+
+async def save_group(
+    chat_id: int,
+    title: str = ""
+):
+
+    await groups_collection.update_one(
+        {
+            "chat_id": chat_id
+        },
+        {
+            "$set": {
+                "chat_id": chat_id,
+                "title": title
+            }
+        },
+        upsert=True
+    )
+
+
+async def delete_group(
+    chat_id: int
+):
+
+    await groups_collection.delete_one(
+        {
+            "chat_id": chat_id
+        }
+    )
+
+
+async def get_all_groups():
+
+    cursor = groups_collection.find(
+        {},
+        {
+            "_id": 0,
+            "chat_id": 1,
+            "title": 1
+        }
+    )
+
+    return await cursor.to_list(
+        length=None
+    )
+
+
+async def get_total_groups():
+
+    return await groups_collection.count_documents({})
 
 
 # =========================================================
